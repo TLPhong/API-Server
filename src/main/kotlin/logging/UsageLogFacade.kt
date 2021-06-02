47b68ef68d2d6fca7417fb75fe5f85a210c012f7
phong <tranloiphong2@gmail.com>
2021-06-03 06:20:48 +0700

    defined usages interface

---
+++ src/main/kotlin/logging/UsageLogFacade.kt
@@ -0,0 +1,27 @@
+package logging
+/*
+Let there be Gallery
+    IS: Collection of Resource,
+    CAN:
+        listing Resource
+        add Resource
+        remove Resource
+    PROPS: number of Resource
+
+Let there be Resource
+     IS: collection of Item
+     PROPS:
+        name
+        number of Item
+        tags
+        created time (epoch)
+        deleted time (epoch)
+
+Let there be Item
+    IS: static media file
+    PROPS: name, index
+    CAN: be serving
+ */
+interface UsageLogFacade {
+
+}
