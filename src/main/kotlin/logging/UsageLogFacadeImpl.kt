f993fbd5587265a4243a5979f9e20764cb954505
phong <tranloiphong2@gmail.com>
2021-07-24 08:39:59 +0700

    add missing file

---
+++ src/main/kotlin/logging/UsageLogFacadeImpl.kt
@@ -0,0 +1,39 @@
+package tlp.media.server.komga.logging
+
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import logging.UsageLogFacade
+import tlp.media.server.komga.logging.entity.Gallery
+import tlp.media.server.komga.logging.entity.Item
+import tlp.media.server.komga.logging.entity.Resource
+import tlp.media.server.komga.logging.entity.impl.ItemImpl
+import java.io.File
+
+class UsageLogFacadeImpl : UsageLogFacade {
+    private val usageLogFile: File = File("usage.log")
+
+    init {
+        usageLogFile.createNewFile()
+    }
+
+    override fun galleryAddResource(gallery: Gallery, resource: Resource) {
+        TODO("Not yet implemented")
+    }
+
+    override fun galleryRemoveResource(gallery: Gallery, resource: Resource) {
+        TODO("Not yet implemented")
+    }
+
+    override fun resourcesBeListing(resource: List<Resource>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun itemsBeListing(items: List<Item>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun itemBeServing(item: Item) {
+        val itemJson = Json.encodeToString(item as ItemImpl)
+        usageLogFile.appendText(itemJson, charset = Charsets.UTF_8)
+    }
+}
