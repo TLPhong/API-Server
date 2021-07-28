4d35840e19839aa6522788665f5edb638d1295a7
phong <tranloiphong2@gmail.com>
2021-07-28 07:51:15 +0700

    Add missing actions params

--- src/main/kotlin/service/UsageLoggingService.kt
+++ src/main/kotlin/service/UsageLoggingService.kt
@@ -3,6 +3,7 @@ package tlp.media.server.komga.service
 import logging.UsageLogFacade
 import mu.KotlinLogging
 import tlp.media.server.komga.logging.UsageLogFacadeImpl
+import tlp.media.server.komga.logging.entity.Action
 import tlp.media.server.komga.logging.toItem
 import tlp.media.server.komga.model.MangaFolder
 import tlp.media.server.komga.model.Page
@@ -16,20 +17,20 @@ class UsageLoggingService {
         val instance = UsageLoggingService()
     }
 
-    private val loggingFacade: UsageLogFacade;
+    private val loggingFacade: UsageLogFacade
     init {
         loggingFacade = UsageLogFacadeImpl()
     }
 
     fun listingPage(pages: List<Pair<Path, Page>>, mangaName: String) {
         logger.info { "Listing pages for $mangaName." }
-        val items = pages.map { it.toItem(mangaName) }
+        val items = pages.map { it.toItem(mangaName, Action.ITEM_LIST) }
         loggingFacade.itemsBeListing(items)
     }
 
     fun servingPage(page: Page, path: Path, mangaName: String){
         logger.info { "Serving item $mangaName" }
-        val item = (path to page).toItem(mangaName)
+        val item = (path to page).toItem(mangaName, Action.ITEM_SERVE)
         loggingFacade.itemBeServing(item)
     }
 
